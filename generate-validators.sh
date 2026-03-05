2a52030bc2a1de39b17d5bb0217eb3bb396c0755
amos <theone.pang@gmail.com>
2026-03-05 20:21:57 +0800

    fix images version
    Signed-off-by: amos <theone.pang@gmail.com>

---
+++ generate-validators.sh
@@ -0,0 +1,4 @@
+besu operator generate-blockchain-config \
+--config-file=qbftConfigFile.json \
+--to=networkFiles \
+--private-key-file-name=key
---
+++ stop.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+docker compose down
+
+rm -rf data/validator1/*
+rm -rf data/validator2/*
+rm -rf data/validator3/*
+rm -rf data/validator4/*
